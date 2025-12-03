e742545f4b86a6f4690cc9946354e906fd024778
Biswadeep Sen <46371546+biswadeep20666@users.noreply.github.com>
2025-12-03 20:14:36 +0530

    Handle decoder channels during autoregressive rollout

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ import os
 import cv2
 import preprocess
 import sys
+
+
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 parser = argparse.ArgumentParser()
@@ -39,26 +41,69 @@ parser.add_argument('--img_width', type=int, default=64)
 parser.add_argument('--img_channel', type=int, default=1)
 
 args = parser.parse_args()
+output_length = args.total_length - args.input_length
 
-mm = MovingMNIST(root=args.root, is_train=True, n_frames_input=10, n_frames_output=10, num_objects=[2])
-train_loader = torch.utils.data.DataLoader(dataset=mm, batch_size=args.batch_size, shuffle=True, drop_last=True,
-                                           num_workers=4)
+if output_length <= 0:
+    raise ValueError("total_length must be greater than input_length to produce forecast frames")
 
-mm = MovingMNIST(root=args.root, is_train=False, n_frames_input=10, n_frames_output=10, num_objects=[2])
-test_loader = torch.utils.data.DataLoader(dataset=mm, batch_size=args.batch_size, shuffle=False, drop_last=True,
-                                          num_workers=4)
+mm = MovingMNIST(
+    root=args.root,
+    is_train=True,
+    n_frames_input=args.input_length,
+    n_frames_output=output_length,
+    num_objects=[2],
+)
+train_loader = torch.utils.data.DataLoader(
+    dataset=mm, batch_size=args.batch_size, shuffle=True, drop_last=True, num_workers=4
+)
+
+mm = MovingMNIST(
+    root=args.root,
+    is_train=False,
+    n_frames_input=args.input_length,
+    n_frames_output=output_length,
+    num_objects=[2],
+)
+test_loader = torch.utils.data.DataLoader(
+    dataset=mm, batch_size=args.batch_size, shuffle=False, drop_last=True, num_workers=4
+)
+
+
+def split_decoder_layers(output, patch_channels):
+    if output.size(2) % patch_channels != 0:
+        raise ValueError(
+            f"Decoder channels ({output.size(2)}) must be divisible by patch channels ({patch_channels})"
+        )
+    layers = output.size(2) // patch_channels
+    return output.view(output.size(0), output.size(1), layers, patch_channels, *output.shape[3:])
+
+
+def rollout_predictions(model, input_tensor, target_length):
+    outputs = []
+    produced = 0
+    patch_channels = args.patch_size * args.patch_size
+    current_input = input_tensor
+
+    while produced < target_length:
+        chunk_output = model(current_input)
+        chunk_frames = min(chunk_output.size(1), target_length - produced)
+        outputs.append(chunk_output[:, :chunk_frames])
+        produced += chunk_frames
+
+        layered_output = split_decoder_layers(chunk_output, patch_channels)
+        current_input = layered_output[:, :, -1, :, :, :]
+
+    return torch.cat(outputs, dim=1)
 
 
 def train_on_batch(input_tensor, target_tensor, encoder, encoder_optimizer, criterion1, criterion2):
- 
-    # input_tensor : torch.Size([batch_size, input_length, 1, 64, 64])
     encoder_optimizer.zero_grad()
-    output_image = encoder(input_tensor)
-    target_tensor = torch.cat([target_tensor] * args.n_decoder_layers, dim=2)
-    loss = 10 * criterion1(output_image, target_tensor) + criterion2(output_image, target_tensor)
+    output_image = rollout_predictions(encoder, input_tensor, target_tensor.size(1))
+    expanded_target = torch.cat([target_tensor] * args.n_decoder_layers, dim=2)
+    loss = 10 * criterion1(output_image, expanded_target) + criterion2(output_image, expanded_target)
     loss.backward()
     encoder_optimizer.step()
-    return loss.item() / target_tensor.size(1)
+    return loss.item() / expanded_target.size(1)
 
 
 def trainIters(encoder, n_epochs, print_every, eval_every):
@@ -74,8 +119,6 @@ def trainIters(encoder, n_epochs, print_every, eval_every):
         loss_epoch = 0
         for i, out in enumerate(train_loader, 0):
             itr += 1
-            # print(itr)
-            # input_batch =  torch.Size([8, 20, 1, 64, 64])
             input_tensor = out[1].to(device)
             input_tensor = preprocess.reshape_patch(input_tensor, args.patch_size)
             target_tensor = out[2].to(device)
@@ -88,10 +131,9 @@ def trainIters(encoder, n_epochs, print_every, eval_every):
             print('epoch ', epoch, ' loss ', loss_epoch, ' epoch time ', time.time() - t0)
 
         if (epoch + 1) % eval_every == 0:
-            mse, mae, ssim = evaluate(encoder, test_loader)
+            mse, mae, ssim_value = evaluate(encoder, test_loader)
             scheduler_enc.step(mse)
-            stats = {}
-            stats['net_param'] = encoder.state_dict()
+            stats = {'net_param': encoder.state_dict()}
             save_dir = os.path.join(args.save_dir, 'epoch-' + str(epoch))
             if not os.path.exists(save_dir):
                 os.makedirs(save_dir)
@@ -101,50 +143,46 @@ def trainIters(encoder, n_epochs, print_every, eval_every):
 
 
 def evaluate(encoder, loader):
-    total_mse, total_mae, total_ssim = 0,0,0
+    total_mse, total_mae, total_ssim = 0, 0, 0
     encoder.eval()
+    patch_channels = args.patch_size * args.patch_size
 
     with torch.no_grad():
         for id, out in enumerate(loader, 0):
-           
-            # input_batch = torch.Size([8, 20, 1, 64, 64])
             input_tensor = out[1].to(device)
             input_tensor = preprocess.reshape_patch(input_tensor, args.patch_size)
             target_tensor = out[2].to(device)
-            predictions = encoder(input_tensor)[:,:,-4:,]
+            target_patched = preprocess.reshape_patch(target_tensor, args.patch_size)
+            predictions_full = rollout_predictions(encoder, input_tensor, target_patched.size(1))
+            layered_output = split_decoder_layers(predictions_full, patch_channels)
+            predictions = layered_output[:, :, -1, :, :, :]
             predictions = preprocess.reshape_patch_back(predictions, args.patch_size)
-            predictions= predictions.cpu().numpy()
+            predictions = predictions.cpu().numpy()
 
             input_tensor = preprocess.reshape_patch_back(input_tensor, args.patch_size)
-            input = input_tensor.cpu().numpy()
-            target = target_tensor.cpu().numpy()
-            
-        
+            input_np = input_tensor.cpu().numpy()
+            target_np = target_tensor.cpu().numpy()
 
-            #save prediction examples
             if id < 20:
                 path = os.path.join(args.gen_frm_dir, str(id))
                 if not os.path.exists(path):
                     os.makedirs(path)
-                for i in range(10):
+                for i in range(args.input_length):
                     name = 'gt' + str(i + 1) + '.png'
-                    # name = str(i + 1).zfill(2) + '.png'
                     file_name = os.path.join(path, name)
-                    img_gt = np.uint8(input[0, i, :, :, :] * 255)
+                    img_gt = np.uint8(input_np[0, i, :, :, :] * 255)
                     img_gt = np.transpose(img_gt, [1, 2, 0])
                     cv2.imwrite(file_name, img_gt)
 
-                for i in range(10):
-                    name = 'gt' + str(i + 11) + '.png'
-                    # name = str(i + 11).zfill(2) + '.png'
+                for i in range(target_np.shape[1]):
+                    name = 'gt' + str(i + 1 + args.input_length) + '.png'
                     file_name = os.path.join(path, name)
-                    img_gt = np.uint8(target[0, i, :, :, :] * 255)
+                    img_gt = np.uint8(target_np[0, i, :, :, :] * 255)
                     img_gt = np.transpose(img_gt, [1, 2, 0])
                     cv2.imwrite(file_name, img_gt)
 
-                for i in range(10):
-                    name = 'pd' + str(i + 11) + '.png'
-                    # name = str(i + 21).zfill(2) + '.png'
+                for i in range(predictions.shape[1]):
+                    name = 'pd' + str(i + 1 + args.input_length) + '.png'
                     file_name = os.path.join(path, name)
                     img_pd = predictions[0, i, :, :, :]
                     img_pd = np.maximum(img_pd, 0)
@@ -153,37 +191,36 @@ def evaluate(encoder, loader):
                     img_pd = np.transpose(img_pd, [1, 2, 0])
                     cv2.imwrite(file_name, img_pd)
 
-
-
-            mse_batch = np.mean((predictions - target) ** 2, axis=(0, 1, 2)).sum()
-            mae_batch = np.mean(np.abs(predictions - target), axis=(0, 1, 2)).sum()
+            mse_batch = np.mean((predictions - target_np) ** 2, axis=(0, 1, 2)).sum()
+            mae_batch = np.mean(np.abs(predictions - target_np), axis=(0, 1, 2)).sum()
             total_mse += mse_batch
             total_mae += mae_batch
 
-            for a in range(0, target.shape[0]):
-                for b in range(0, target.shape[1]):
-                    total_ssim += ssim(target[a, b, 0,], predictions[a, b, 0,]) / (target.shape[0] * target.shape[1])
+            for a in range(0, target_np.shape[0]):
+                for b in range(0, target_np.shape[1]):
+                    total_ssim += ssim(target_np[a, b, 0,], predictions[a, b, 0,]) / (target_np.shape[0] * target_np.shape[1])
 
-
-    print('eval mse ', total_mse / len(loader), ' eval mae ', total_mae / len(loader), ' eval ssim ',
-          total_ssim / len(loader))
+    print('eval mse ', total_mse / len(loader), ' eval mae ', total_mae / len(loader), ' eval ssim ', total_ssim / len(loader))
     return total_mse / len(loader), total_mae / len(loader), total_ssim / len(loader)
+
 
 print('BEGIN TRAIN')
 
 model = get_model(args).to(device)
 model = nn.DataParallel(model).to(device)
+
+
 def count_parameters(model):
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
 
-print('encoder ', count_parameters(model))
-# sys.exit()
+
+total_params = count_parameters(model)
+print('encoder ', total_params)
+
 if args.checkpoint_path != '':
     print('load model:', args.checkpoint_path)
     stats = torch.load(args.checkpoint_path, map_location=torch.device('cpu'))
     model.load_state_dict(stats['net_param'])
-    # plot_losses = trainIters(model, args.n_epochs, print_every=args.print_every, eval_every=args.eval_every)
-    mse, mae, ssim = evaluate(model, test_loader)
+    mse, mae, ssim_value = evaluate(model, test_loader)
 else:
-    # mse, mae, ssim = evaluate(model, test_loader)
     plot_losses = trainIters(model, args.n_epochs, print_every=args.print_every, eval_every=args.eval_every)
